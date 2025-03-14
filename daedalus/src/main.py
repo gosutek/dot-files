ddd3cc6761c375e9bb1156092610f568699baf55
GosuTek <gosutek@gmail.com>
2025-03-14 16:17:39 +0200

    rudimentary styling

--- daedalus/src/main.py
+++ daedalus/src/main.py
@@ -1,7 +1,9 @@
 from fabric import Application
+from fabric.utils import get_relative_path
 from fabric.widgets.label import Label
 from fabric.widgets.wayland import Window
-from fabric.hyprland.widgets import Workspaces
+from fabric.widgets.centerbox import CenterBox
+from fabric.widgets.box import Box
 
 
 class Bar(Window):
@@ -14,18 +16,36 @@ class Bar(Window):
             exclusivity="auto",
             visible=True,
             all_visible=True,
-            child=Label("Hello, World"),
         )
 
-        self.workspaces = Workspaces(
-            name="workspaces",
-            invert_scroll=True,
-            empty_scroll=True,
-            v_align="fill",
+        self.bar_inner = CenterBox(
+            name="bar-inner",
             orientation="h",
-            spacing=10,
+            h_align="fill",
+            v_align="center",
+            start_children=Box(
+                name="start-container",
+                spacing=4,
+                orientation="h",
+                children=[Label("I am a child of the start")],
+            ),
+            end_children=Box(
+                name="end-container",
+                spacing=4,
+                orientation="h",
+                children=[Label("I am a child of the end")],
+            ),
         )
 
+        self.children = self.bar_inner
 
-app = Application("default", Bar())
-app.run()
+
+if __name__ == "__main__":
+    app = Application("default", Bar())
+
+    def set_css():
+        app.set_stylesheet_from_file(get_relative_path("main.css"))
+
+    app.set_css = set_css
+    app.set_css()
+    app.run()
