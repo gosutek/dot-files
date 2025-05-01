dc1d6701191302a6dff00d3cbc7bf21dc2800225
GosuTek <gosutek@gmail.com>
2025-05-02 02:52:15 +0300

    gtk 3.0 fish kitty starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-
 set -U fish_greeting
+set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+set -x KITTY_CONFIG_DIRECTORY ~/.config/kitty/
 starship init fish | source
-nitch
+fastfetch
 
 thefuck --alias | source
