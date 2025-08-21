33aa657714804ff735e6e9a27c0414c35f6ee697
GosuTek <gosutek@gmail.com>
2025-08-21 20:24:50 +0300

    include quickshell/

--- sync.sh
+++ sync.sh
@@ -2,3 +2,4 @@
 
 rsync -av --delete "$HOME/.config/hypr/" "$HOME/projects/dot-files/.config/hypr/"
 rsync -av --delete "$HOME/.config/fastfetch/" "$HOME/projects/dot-files/.config/fastfetch/"
+rsync -av --delete "$HOME/.config/quickshell/" "$HOME/projects/dot-files/.config/quickshell/"
