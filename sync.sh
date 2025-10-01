582177c8d343de5e0856ac1879ae14a9d7ee9c51
GosuTek <gosutek@gmail.com>
2025-10-01 21:17:43 +0300

    add: initial neovim configuration from kickstart.nvim

--- sync.sh
+++ sync.sh
@@ -3,3 +3,4 @@
 rsync -av --delete "$HOME/.config/hypr/" "$HOME/projects/dot-files/.config/hypr/"
 rsync -av --delete "$HOME/.config/fastfetch/" "$HOME/projects/dot-files/.config/fastfetch/"
 rsync -av --delete "$HOME/.config/quickshell/" "$HOME/projects/dot-files/.config/quickshell/"
+rsync -av --delete "$HOME/.config/leafvim/" "$HOME/projects/dot-files/.config/leafvim"
